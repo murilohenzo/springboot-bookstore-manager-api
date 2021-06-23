d53c0b44dbb7ad2cdc8c0224cf6d8812860fdbe9
murilohenzo <murilohezo@gmail.com>
2021-06-23 12:26:39 -0300

    created auditable entity

--- src/main/java/com/murilohenzo/bookstoremanager/books/entity/Book.java
+++ src/main/java/com/murilohenzo/bookstoremanager/books/entity/Book.java
@@ -1,6 +1,7 @@
 package com.murilohenzo.bookstoremanager.books.entity;
 
 import com.murilohenzo.bookstoremanager.author.entity.Author;
+import com.murilohenzo.bookstoremanager.entity.Auditable;
 import com.murilohenzo.bookstoremanager.publishers.entity.Publisher;
 import com.murilohenzo.bookstoremanager.users.entity.User;
 import lombok.Data;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 
 @Data
 @Entity
-public class Book {
+public class Book extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/murilohenzo/bookstoremanager/publishers/entity/Publisher.java
+++ src/main/java/com/murilohenzo/bookstoremanager/publishers/entity/Publisher.java
@@ -1,6 +1,7 @@
 package com.murilohenzo.bookstoremanager.publishers.entity;
 
 import com.murilohenzo.bookstoremanager.books.entity.Book;
+import com.murilohenzo.bookstoremanager.entity.Auditable;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Data
 @Entity
-public class Publisher {
+public class Publisher extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
